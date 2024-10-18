e7c15815888d6d1035b9e4d94c9a56f7828d5730
Mile2617 <milena.guzman@udla.edu.ec>
2024-10-18 08:04:15 -0500

    codigo1

--- funciones.h
+++ funciones.h
@@ -1,3 +1,4 @@
 void inventario(char productos[10][20], float precios[10]);
 void calculos(char productos[10][20], float precios[10]);
 void buscarProducto(char productos[10][20], float precios[10]);
+
