11c0a8a3ce6ea6a4d3c72c0a9c85fa3b2fc28855
Mile2617 <157331549+Mile2617@users.noreply.github.com>
2024-10-17 21:22:10 -0500

    main.c

--- main.c
+++ main.c
@@ -6,8 +6,6 @@
 int main() {
     char nombproducto[10][20];
     float precios[10];
-
-    // Llamar a las funciones para ejecutar el flujo del programa
     inventario(nombproducto, precios);
     calculos(nombproducto, precios);
     buscarproducto(nombproducto, precios);
